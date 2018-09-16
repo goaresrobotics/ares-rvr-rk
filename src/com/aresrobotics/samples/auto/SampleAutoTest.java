ec76809f747356c03bb3092f6ebcf35ad056f741
Michael Lewis <mabeeenigma@gmail.com>
2018-09-15 18:22:35 -0700

    file renames

--- src/com/aresrobotics/samples/auto/SampleAutoTest.java
+++ src/com/aresrobotics/samples/auto/SampleAutoTest.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "Motor Control")
-public class SampleMotorControl extends LinearOpMode {
+@Autonomous(name = "Sample: Auto Test", group = "Samples")
+public class SampleAutoTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor motor = hardwareMap.dcMotor.get("frontLeft");
