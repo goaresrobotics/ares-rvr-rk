fb5f69c84e3ad2942e038ab27b9058a12a843b8d
Michael Lewis <mabeeenigma@gmail.com>
2018-11-07 21:40:16 -0800

    adds auto drive using encoder sample with external robot hardware class

---
+++ src/com/aresrobotics/library/hardware/AresSampleRobot.java
@@ -0,0 +1,61 @@
+package com.aresrobotics.library.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ *
+ * Sample of implementing sample hardware for a first round of an ARES robot
+ */
+public class AresSampleRobot
+{
+    /* Public OpMode members. */
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+    public DcMotor  leftArm     = null;
+//    public Servo    leftClaw    = null;
+//    public Servo    rightClaw   = null;
+
+
+//    public static final double MID_SERVO       =  0.5 ;
+//    public static final double ARM_UP_POWER    =  0.45 ;
+//    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public AresSampleRobot(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        // Going to set to run to a position - as that is the initially expected use case
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        // Define and initialize ALL installed servos.  - no servos on the bot
+//        leftClaw  = hwMap.get(Servo.class, "left_hand");
+//        rightClaw = hwMap.get(Servo.class, "right_hand");
+//        leftClaw.setPosition(MID_SERVO);
+//        rightClaw.setPosition(MID_SERVO);
+    }
+}
