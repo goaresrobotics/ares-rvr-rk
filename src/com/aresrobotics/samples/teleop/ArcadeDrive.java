c0e4fd4017d7102f6960eb61f2afed930fdb5549
ACME Robotics <goacmerobotics@gmail.com>
2018-11-15 22:00:28 -0800

    Got teleop code working

--- src/com/aresrobotics/samples/teleop/ArcadeDrive.java
+++ src/com/aresrobotics/samples/teleop/ArcadeDrive.java
@@ -1,11 +1,12 @@
 package com.aresrobotics.samples.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.aresrobotics.library.hardware.AresSampleRobot;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 @TeleOp(name = "Drive")
 public class ArcadeDrive extends OpMode {
@@ -14,7 +15,9 @@ public class ArcadeDrive extends OpMode {
     private DcMotor motorLeft;
     private DcMotor spinner;
     private DcMotor slides;
-    Servo intake;
+    private Servo intake;
+    //DigitalChannel touchMe;
+    //DigitalChannel touchMeToo;
 
     static final double INCREMENT   = 0.02;
     static final int    CYCLE_MS    =   50;
@@ -22,7 +25,6 @@ public class ArcadeDrive extends OpMode {
     static final double MIN_POS     =  0.0;
 
     double  position = MAX_POS;
-    boolean rampUp = true;
 
     private final void sleep(long milliseconds) {
         try {
@@ -38,11 +40,16 @@ public class ArcadeDrive extends OpMode {
 
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
- //       spinner = hardwareMap.dcMotor.get("spinner");
- //       slides = hardwareMap.dcMotor.get("slides");
+        spinner = hardwareMap.dcMotor.get("spinner");
+        slides = hardwareMap.dcMotor.get("slides");
         intake = hardwareMap.servo.get("intake");
+        //touchMe = hardwareMap.get(DigitalChannel.class, "touchMe");
+        //touchMeToo = hardwareMap.get(DigitalChannel.class, "touchMeToo");
 
+        intake.setPosition(position);
 
+        //touchMe.setMode(DigitalChannel.Mode.INPUT);
+        //touchMeToo.setMode(DigitalChannel.Mode.INPUT);
     }
 
     @Override
@@ -57,29 +64,38 @@ public class ArcadeDrive extends OpMode {
         motorRight.setPower(right);
         motorLeft.setPower(left);
 
-      //  double intakeSlide = gamepad2.left_stick_y;
+        slides.setPower(gamepad2.left_stick_y/2);
 
-      //  slides.setPower(intakeSlide);
+        /*if (touchMe.getState() == true) {
+            slides.setPower(-0.1); }
+            else {
+            slides.setPower(intakeSlide/15); }
 
-        if (rampUp && gamepad2.left_bumper) {
+
+        if (touchMeToo.getState() == true) {
+            slides.setPower(0.1); }
+        else {
+            slides.setPower(intakeSlide/15); }*/
+
+
+        if (gamepad2.left_bumper) {
             position += INCREMENT ;
             if (position >= MAX_POS ) {
                 position = MAX_POS;
-                rampUp = false;  // Switch ramp direction
             }
         }
-        if (!rampUp && gamepad2.right_bumper) {
+        if (gamepad2.right_bumper) {
             position -= INCREMENT ;
             if (position <= MIN_POS ) {
                 position = MIN_POS;
-                rampUp = true;  // Switch ramp direction
             }
         }
 
             intake.setPosition(position);
             sleep(CYCLE_MS);
 
+        spinner.setPower(gamepad2.right_stick_y);
 
+        }
     }
 
-}
